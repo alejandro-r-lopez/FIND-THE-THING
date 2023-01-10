fa6b15bc767c3e9571e5a3955b82edd57fe36385
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-10 14:58:56 -0500

    Added HTML elemnts

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 /* Imports */
 
 /* Get DOM Elements */
+const cupOne = document.getElementById('cup-one');
+const cupTwo = document.getElementById('cup-two');
+const cupThree = document.getElementById('cup-three');
+
+const ballOne = document.getElementById('ball-one');
+const ballTwo = document.getElementById('ball-two');
+const ballThree = document.getElementById('ball-three');
 
 /* State */
 
